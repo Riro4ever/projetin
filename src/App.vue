2aa5bb4e9f79fea932bd43461c0acfb55d65b1c2
Ricardo Araujo <ricardoriro@live.com>
2023-07-12 14:46:13 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/Test.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Vite + Vue" />
+  <Test msg="Test"/>
+  <!-- Implementar roteamento e paginas para testar -->
+</template>
+
---
+++ src/components/Test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="example">{{ msg }}</div>
+  <div class="example">{{ helloMessage }}</div>
+</template>
+  
+<script>
+export default {
+  data() {
+    return {
+      helloMessage: 'Hello world!'
+    }
+  },
+  props: {
+    msg: String
+  },
+  mounted() {
+    alert(import.meta.env.VITE_TEST);
+  }
+  
+}
+</script>
